5230ce4cd2118034bb7497f9f5629225affca3c2
Sebastian Sanchez <sebastiansaor22@gmail.com>
2023-01-05 16:30:26 -0500

    Dockerizando app y web-server

--- app/main.py
+++ app/main.py
@@ -1,27 +1,25 @@
 import charts
 import utils
 import read_csv
+import pandas as pd
 
-data = [
-    {
-        'Country':'Colombia',
-        'Population':500
-    },
-    {
-        'Country':'Bolivia',
-        'Population':300
-    }
-]
 
 def run():
+    '''
     data = read_csv.read_csv('data.csv')
     data = list(filter(lambda item : item['Continent'] == 'South America', data))
 
     countries = list(map(lambda x: x['Country'], data))
     percentages = list(map(lambda x: x['World Population Percentage'], data))
-    charts.generate_pie_chart(countries, percentages)
+    '''
+    df = pd.read_csv('data.csv')
+    df = df[df['Continent'] == 'Africa']
 
+    countries = df['Country'].values
+    percentages = df['World Population Percentage'].values
+    charts.generate_pie_chart(countries, percentages)
     
+    data = read_csv.read_csv('data.csv')
     country = input('Type country => ')
 
     result = utils.get_population_by_country(data, country)
